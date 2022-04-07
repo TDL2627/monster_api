3a1a0f4daf276cf46f26bba7befe6b76fece04d8
TDL2627 <the.dark.lord.2627@gmail.com>
2022-04-07 15:21:37 +0200

    added routes

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
 const express = require('express');
-const pool = require('./db')
+const monsters = require('./routes/monsters')
+
 const app = express();
 
 
-
-app.get('/monsters',(request, response, next)=>{
-    pool.query('SELECT * FROM monsters ORDER BY id ASC', (err, res)=>{
-        if(err) next(err);
-
-        response.json(res.rows);
-        });
-});
+app.use('/monsters', monsters)
 
 
-app.get('/monsters/:id',(request, response, next)=>{
-const{ id}=request.params;
-pool.query('SELECT * FROM monsters WHERE id = $1', [id], (err, res) =>{
-if(err) return next(err);
-response.json(res.rows);
-});
-});
+
 
 
 app.use((err, req, res, next)=>{
