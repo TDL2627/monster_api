18d92b3350f7ce42a707cef4626ad57c1b26b5c0
TDL2627 <the.dark.lord.2627@gmail.com>
2022-04-07 19:23:13 +0200

    added habitat route andall CRUD opps added

--- routes/habitats.js
+++ routes/habitats.js
@@ -5,53 +5,50 @@ const router = Router();
 
 // get all
 router.get('/',(request, response, next)=>{
-    pool.query('SELECT * FROM monsters ORDER BY id ASC', (err, res)=>{
+    pool.query('SELECT * FROM habitats ORDER BY id ASC', (err, res)=>{
         if(err) next(err);
-
         response.json(res.rows);
     });
 });
 
 // get 1
-
 router.get('/:id',(request, response, next)=>{
     const { id } = request.params;
-    pool.query('SELECT * FROM monsters WHERE id = $1', [id], (err, res) =>{
+    pool.query('SELECT * FROM habitats WHERE id = $1', [id], (err, res) =>{
     if(err) return next(err);
     response.json(res.rows);
     });
 });
 
-
 // post
 router.post('/',(request, response, next)=>{
-    const {name, personality} = request.body;
+    const {name, climate, temperature} = request.body;
     pool.query(
-        'INSERT INTO monsters(name, personality) VALUES($1, $2)',
-        [name, personality],
+        'INSERT INTO habitats(name, climate, temperature) VALUES($1, $2, $3)',
+        [name, climate, temperature],
         (err, res)=>{
             if(err) return next(err);
-            response.json('Posted monster');
+            response.json('Posted habitat');
         });
 });
 
 // update
 router.put('/:id',(request, response, next)=>{
     const{ id }=request.params;
-    const {name, personality}=request.body;
-    const keys = ['name','personality'];
+    const {name, climate, temperature} = request.body;
+    const keys = ['name', 'climate', 'temperature'];
      const fields = [];
 
      keys.forEach(key =>{
          if(request.body[key]) fields.push(key);
      });
     fields.forEach((field, index) =>{ pool.query(
-        `UPDATE monsters SET ${field}=($1) WHERE id = ($2)`,
+        `UPDATE habitats SET ${field}=($1) WHERE id = ($2)`,
         [request.body[field], id],
         (err, res)=>{
             if(err) return next(err);
          if(index === fields.length - 1)
-            response.json('Updated monster');
+            response.json('Updated habitat');
         }
     );});
 });
@@ -61,10 +58,10 @@ router.delete('/:id',(request, response, next) =>{
     const {id} = request.params;
 
     pool.query(
-        'DELETE FROM monsters WHERE id=($1)',
+        'DELETE FROM habitats WHERE id=($1)',
         [id],(err, res)=>{
             if(err) return next(err);
-            response.json('DELETED monster');
+            response.json('DELETED habitat');
         }
     )
 });
